d317a59f57b02d529366444132a1a9c4a15faef6
gram <git@orsinium.dev>
2025-08-16 09:38:27 +0200

    draft all retry funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
+#![feature(linked_list_retain)]
 #![allow(clippy::new_without_default)]
+#![allow(clippy::missing_safety_doc)]
 extern crate alloc;
 mod actor;
 pub mod retries;
--- src/retries.rs
+++ src/retries.rs
@@ -1,14 +1,10 @@
-use core::cell::{LazyCell, RefCell};
-
 use crate::*;
-use alloc::collections::VecDeque;
-use alloc::{boxed::Box, vec::Vec};
+use alloc::boxed::Box;
+use alloc::collections::LinkedList;
+use core::cell::{LazyCell, RefCell};
 use critical_section::Mutex;
+use esp_hal::delay;
 use esp_wifi_sys::include::*;
-use portable_atomic::{AtomicBool, AtomicU8, Ordering};
-
-static ESP_NOW_SEND_CB_INVOKED: AtomicBool = AtomicBool::new(false);
-static ESP_NOW_SEND_STATUS: AtomicBool = AtomicBool::new(true);
 
 struct Msg {
     addr: Addr,
@@ -16,12 +12,28 @@ struct Msg {
     attempts: u8,
 }
 
-type Queue = VecDeque<Msg>;
+type List = LinkedList<Msg>;
 
-static QUEUE: Mutex<LazyCell<RefCell<Queue>>> =
-    Mutex::new(LazyCell::new(|| RefCell::new(Queue::new())));
+static QUEUE: Mutex<LazyCell<RefCell<List>>> =
+    Mutex::new(LazyCell::new(|| RefCell::new(List::new())));
+
+pub fn start() {
+    _ = unsafe { esp_now_register_send_cb(Some(send_cb)) };
+}
+
+pub fn stop() {
+    _ = unsafe { esp_now_register_send_cb(None) };
+    critical_section::with(|cs| {
+        let queue = QUEUE.borrow(cs);
+        let mut queue = queue.borrow_mut();
+        queue.clear();
+    });
+}
 
 pub fn send(addr: Addr, data: &[u8]) -> i32 {
+    while pending(addr) {
+        delay::Delay::new().delay_micros(5);
+    }
     let code = unsafe { esp_now_send(addr.as_ptr(), data.as_ptr(), data.len()) };
     if code == 0 {
         critical_section::with(|cs| {
@@ -41,11 +53,7 @@ fn confirm(addr: Addr) {
     critical_section::with(|cs| {
         let queue = QUEUE.borrow(cs);
         let mut queue = queue.borrow_mut();
-        let maybe = queue.iter().enumerate().find(|(_, item)| addr == item.addr);
-        let Some((i, _)) = maybe else {
-            return;
-        };
-        queue.remove(i);
+        queue.retain(|item| addr != item.addr);
     });
 }
 
@@ -66,16 +74,23 @@ fn retry(addr: Addr) {
             return;
         };
         msg.attempts += 1;
-        let data = &msg.data;
-        // TODO: mave it outside CS.
-        unsafe { esp_now_send(addr.as_ptr(), data.as_ptr(), data.len()) };
+        if msg.attempts >= 3 {
+            queue.retain(|item| addr != item.addr);
+        } else {
+            let data = &msg.data;
+            // TODO: move it outside CS.
+            unsafe { esp_now_send(addr.as_ptr(), data.as_ptr(), data.len()) };
+        }
     })
 }
 
-pub unsafe extern "C" fn send_cb(_mac_addr: *const u8, status: esp_now_send_status_t) {
-    critical_section::with(|_| {
-        let is_ok = status == esp_now_send_status_t_ESP_NOW_SEND_SUCCESS;
-        ESP_NOW_SEND_STATUS.store(is_ok, Ordering::Relaxed);
-        ESP_NOW_SEND_CB_INVOKED.store(true, Ordering::Release);
-    })
+unsafe extern "C" fn send_cb(addr: *const u8, status: esp_now_send_status_t) {
+    let is_ok = status == esp_now_send_status_t_ESP_NOW_SEND_SUCCESS;
+    let addr = core::slice::from_raw_parts(addr, 6);
+    let addr: Addr = addr.try_into().unwrap();
+    if is_ok {
+        confirm(addr);
+    } else {
+        retry(addr);
+    }
 }
